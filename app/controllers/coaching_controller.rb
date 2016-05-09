dbe7a2c6ea53ed974644973282ee87700c85e153
Laurence Crevel <laurence@a-nounou.com>
2016-05-09 14:16:05 +0200

    Stupid coaching done.

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,21 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    @reply = coach_answer(@query)
+  end
+
+
+  def ask
+  end
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return "Good boy!"
+    elsif your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
